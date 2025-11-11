013223956f7b283076b36f9ac5d3e4d2eef2b985
flam3yx <154548576+flam3yx@users.noreply.github.com>
2025-11-11 19:16:04 +0100

    Implement cooldown system for commands and add new commands: dog, gif, echo, info, reload

--- commands/utility/pingPong.js
+++ commands/utility/pingPong.js
@@ -1,9 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
-//so they can be read by other files
+
 module.exports = {
-    //provide the command definition shown above for registering to Discord
+	cooldown: 5,
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
-    //contain the functionality to run from our event handler when the command is used
 	async execute(interaction) {
 		await interaction.reply('Pong!');
 	},
